2c623198c3ebd443aeb546ae6476d088d72186b3
Piotr M 🚂 <piotrrgw@gmail.com>
2026-04-19 23:36:29 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,5 @@
-// Wersja: v3.7
-// Numer wersji musi być zmieniany przy każdej aktualizacji plików aplikacji
-const CACHE_NAME = 'kalkulator-ph-v3.11.1';
+// Wersja aplikacji: v3.11.2
+const CACHE_NAME = 'kalkulator-ph-v3.11.2';
 
 // Najważniejsze pliki niezbędne do działania offline
 const ASSETS = [
@@ -9,18 +8,14 @@ const ASSETS = [
   './manifest.json',
   './zespoly_trakcyjne.json',
   './favicon.svg',
-  './apple-touch-icon.png',
-  './icon-192x192.png',
-  './icon-512x512.png'
+  './apple-touch-icon.png'
 ];
 
 // 1. Instalacja i bezpieczne cache'owanie zasobów
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Zapisywanie plików do pamięci v3.7...');
-      // Używamy mapowania z catch, aby brak jednego pliku (np. ikony) 
-      // nie zablokował zapisania całego kalkulatora do działania offline.
+      console.log('[Service Worker] Zapisywanie plików do pamięci v3.11.2...');
       return Promise.all(
         ASSETS.map(asset => {
           return cache.add(asset).catch(error => {
@@ -30,7 +25,6 @@ self.addEventListener('install', (e) => {
       );
     })
   );
-  // UWAGA: Usunięto sztywne self.skipWaiting(), aby aktualizacja następowała po kliknięciu w baner
 });
 
 // Nasłuchiwanie na polecenie wymuszenia aktualizacji ze strony użytkownika (kliknięcie Aktualizuj)
@@ -45,7 +39,6 @@ self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        // Jeśli nazwa cache w przeglądarce nie pasuje do aktualnego CACHE_NAME, usuwamy go
         if (key !== CACHE_NAME) {
           console.log('[Service Worker] Usuwanie starej wersji cache:', key);
           return caches.delete(key);
@@ -53,7 +46,6 @@ self.addEventListener('activate', (e) => {
       }));
     })
   );
-  // Od razu po aktywacji Service Worker zaczyna kontrolować stronę
   self.clients.claim();
 });
 
@@ -61,9 +53,7 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
-      // Zwróć plik z cache, a jeśli go tam nie ma, pobierz z Internetu
       return response || fetch(e.request).catch(() => {
-        // Blok wyciszający błędy (np. niedostępność skryptu Google Tag Manager w trybie offline)
         console.log('[Service Worker] Brak dostępu do sieci. Ignorowanie żądania dla:', e.request.url);
       });
     })
